68799dfb958b9e4d2e97bfd4fcffb5af4f9bb995
huamin <huamin@carslan.com.cn>
2026-06-04 09:31:22 +0800

    初始化提交

---
+++ hot.sql
@@ -0,0 +1,352 @@
+/*
+SQLyog v10.2 
+MySQL - 8.4.0 : Database - hot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `hot`;
+
+/*Table structure for table `sys_butt` */
+
+CREATE TABLE `sys_butt` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '按钮管理',
+  `menu_id` int DEFAULT NULL COMMENT '菜单id',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '名称',
+  `sign` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '路由',
+  `icon` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '图标',
+  `sort` int NOT NULL COMMENT '排序',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_butt` */
+
+/*Table structure for table `sys_butt_role` */
+
+CREATE TABLE `sys_butt_role` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_butt_role` */
+
+/*Table structure for table `sys_butt_role_data` */
+
+CREATE TABLE `sys_butt_role_data` (
+  `butt_id` int NOT NULL,
+  `butt_role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_butt_role_data` */
+
+/*Table structure for table `sys_data` */
+
+CREATE TABLE `sys_data` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_data` */
+
+/*Table structure for table `sys_data_dep` */
+
+CREATE TABLE `sys_data_dep` (
+  `data_id` int NOT NULL,
+  `dep_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_data_dep` */
+
+/*Table structure for table `sys_data_inv` */
+
+CREATE TABLE `sys_data_inv` (
+  `data_id` int NOT NULL,
+  `inv_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_data_inv` */
+
+/*Table structure for table `sys_data_mdep` */
+
+CREATE TABLE `sys_data_mdep` (
+  `data_id` int NOT NULL COMMENT '管理部门',
+  `dep_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_data_mdep` */
+
+/*Table structure for table `sys_dict` */
+
+CREATE TABLE `sys_dict` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '字典',
+  `value` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_czech_ci NOT NULL COMMENT '数据值',
+  `label` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_czech_ci NOT NULL COMMENT '标签名',
+  `type` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_czech_ci NOT NULL COMMENT '类型',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序（升序）',
+  `del_flag` char(1) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `value` (`value`,`type`,`del_flag`),
+  KEY `label` (`label`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin COMMENT='字典表';
+
+/*Data for the table `sys_dict` */
+
+/*Table structure for table `sys_log` */
+
+CREATE TABLE `sys_log` (
+  `url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `msg` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `ip` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `userid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_log` */
+
+/*Table structure for table `sys_menu` */
+
+CREATE TABLE `sys_menu` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '菜单管理',
+  `parent_id` int NOT NULL COMMENT '父级编号',
+  `parent_ids` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin COMMENT '所有父级编号',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '名称',
+  `path` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '路由',
+  `component` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '路由组件',
+  `sign` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '组件标识',
+  `icon` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '图标',
+  `sort` int NOT NULL COMMENT '排序',
+  `type` int DEFAULT NULL COMMENT '菜单类型 1:PC端，2:移动端',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '创建用户',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '修改用户',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_menu` */
+
+/*Table structure for table `sys_menu_role` */
+
+CREATE TABLE `sys_menu_role` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_menu_role` */
+
+/*Table structure for table `sys_menu_role_data` */
+
+CREATE TABLE `sys_menu_role_data` (
+  `menu_id` int NOT NULL,
+  `menu_role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_menu_role_data` */
+
+/*Table structure for table `sys_modu` */
+
+CREATE TABLE `sys_modu` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '模块管理',
+  `menu_id` int DEFAULT NULL COMMENT '菜单id',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '名称',
+  `path` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '路由',
+  `component` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '路由组件',
+  `sign` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '组件标识',
+  `icon` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '图标',
+  `sort` int NOT NULL COMMENT '排序',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_modu` */
+
+/*Table structure for table `sys_modu_role` */
+
+CREATE TABLE `sys_modu_role` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_modu_role` */
+
+/*Table structure for table `sys_modu_role_data` */
+
+CREATE TABLE `sys_modu_role_data` (
+  `modu_id` int NOT NULL,
+  `modu_role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_modu_role_data` */
+
+/*Table structure for table `sys_role` */
+
+CREATE TABLE `sys_role` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '名称',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_role` */
+
+/*Table structure for table `sys_role_butt` */
+
+CREATE TABLE `sys_role_butt` (
+  `role_id` int NOT NULL,
+  `butt_role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_role_butt` */
+
+/*Table structure for table `sys_role_data` */
+
+CREATE TABLE `sys_role_data` (
+  `role_id` int NOT NULL,
+  `data_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_role_data` */
+
+/*Table structure for table `sys_role_menu` */
+
+CREATE TABLE `sys_role_menu` (
+  `role_id` int NOT NULL,
+  `menu_role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_role_menu` */
+
+/*Table structure for table `sys_role_modu` */
+
+CREATE TABLE `sys_role_modu` (
+  `role_id` int NOT NULL,
+  `modu_role_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_role_modu` */
+
+/*Table structure for table `sys_user` */
+
+CREATE TABLE `sys_user` (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '姓名',
+  `login_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '登陆名',
+  `pass_word` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '密码',
+  `per_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '人员编码',
+  `depname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '部门',
+  `user_type` int DEFAULT NULL COMMENT '用户类型',
+  `login_ip` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '最后登陆IP',
+  `login_date` datetime DEFAULT NULL COMMENT '最后登陆时间',
+  `login_flag` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '1' COMMENT '是否可登录 0:否 1:是 2:锁定',
+  `passw_time` datetime DEFAULT NULL COMMENT '密码修改时间',
+  `error_time` datetime DEFAULT NULL COMMENT '登录错误时间',
+  `error_count` int NOT NULL DEFAULT '0' COMMENT '登录错误次数',
+  `menu_role` int NOT NULL DEFAULT '0' COMMENT '菜单权限 1：所有权限，0：指定权限',
+  `modu_role` int NOT NULL DEFAULT '0' COMMENT '模块权限 1：所有权限，0：指定权限',
+  `butt_role` int NOT NULL DEFAULT '0' COMMENT '按钮权限 1：所有权限，0：指定权限',
+  `data_role` int NOT NULL DEFAULT '0' COMMENT '数据权限 1：所有权限，0：指定权限',
+  `create_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '创建用户',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '修改用户',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`name`,`login_name`,`pass_word`,`per_code`,`depname`,`user_type`,`login_ip`,`login_date`,`login_flag`,`passw_time`,`error_time`,`error_count`,`menu_role`,`modu_role`,`butt_role`,`data_role`,`create_user`,`create_time`,`update_user`,`update_time`,`remarks`,`del_flag`) values ('1','admin','admin','$2a$10$3PVyzgaN0g1y5qOjTWxOOOZgI0/8UaUtk0r3dgwsjYtV9F0e.Lytu',NULL,NULL,NULL,'0:0:0:0:0:0:0:1','2026-06-03 19:53:20','1',NULL,NULL,0,0,0,0,0,NULL,NULL,NULL,NULL,NULL,'0');
+
+/*Table structure for table `sys_user_butt` */
+
+CREATE TABLE `sys_user_butt` (
+  `user_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `butt_role_id` int NOT NULL,
+  KEY `user_id` (`user_id`,`butt_role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_user_butt` */
+
+/*Table structure for table `sys_user_data` */
+
+CREATE TABLE `sys_user_data` (
+  `user_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `data_id` int NOT NULL,
+  KEY `user_id` (`user_id`,`data_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_user_data` */
+
+/*Table structure for table `sys_user_menu` */
+
+CREATE TABLE `sys_user_menu` (
+  `user_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `menu_role_id` int NOT NULL,
+  KEY `user_id` (`user_id`,`menu_role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_user_menu` */
+
+/*Table structure for table `sys_user_modu` */
+
+CREATE TABLE `sys_user_modu` (
+  `user_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `modu_role_id` int NOT NULL,
+  KEY `user_id` (`user_id`,`modu_role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+/*Data for the table `sys_user_modu` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
